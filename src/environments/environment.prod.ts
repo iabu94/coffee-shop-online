99bb56541df17ef2cbab7d992ca2bc0658f6f4c6
Abdullah Mansoor <mansoorabdullah001@gmail.com>
2022-01-27 02:39:27 +0530

    new env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   firebase: {
-    projectId: 'coffee-shop-385a5',
+    projectId: 'coffee-shop-68531',
     appId: '1:45047380208:web:6782e9c04c3964b8acaa35',
-    storageBucket: 'coffee-shop-385a5.appspot.com',
+    storageBucket: 'coffee-shop-68531.appspot.com',
     locationId: 'us-central',
     apiKey: 'AIzaSyBRs5IMuDSBmaXV8Mau-9kcrklUwPEDDaY',
-    authDomain: 'coffee-shop-385a5.firebaseapp.com',
+    authDomain: 'coffee-shop-68531.firebaseapp.com',
     messagingSenderId: '45047380208',
     measurementId: 'G-H9MNLC9E84',
   },
-  production: true
+  production: true,
 };
